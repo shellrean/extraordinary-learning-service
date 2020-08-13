2a7d55edb19d66fcf32be31908d392d587ca55a7
shellrean <reanyusufali@gmail.com>
2020-08-13 11:06:37 +0700

    comment class request

--- app/Http/Requests/ClassroomLiveComment.php
+++ app/Http/Requests/ClassroomLiveComment.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CommentRequest extends FormRequest
+class ClassroomLiveComment extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class CommentRequest extends FormRequest
     public function rules()
     {
         return [
-            'lecture_id'        => 'required|exists:lectues,id',
+            'classlive_id'        => 'required|exists:classroom_live_comments,id',
             'content'   => 'required'
         ];
     }
--- database/migrations/2020_08_13_110400_create_classroom_live_comments_table.php
+++ database/migrations/2020_08_13_110400_create_classroom_live_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateClassroomLiveCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('classroom_live_comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('lecture_id');
+            $table->unsignedBigInteger('classroom_live_id');
             $table->unsignedBigInteger('user_id');
             $table->text('content');
             $table->timestamps();
 
-            $table->foreign('lecture_id')->references('id')->on('lectures')->onDelete('cascade');
+            $table->foreign('classroom_live_id')->references('id')->on('classroom_lives')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
@@ -32,6 +32,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('classroom_live_comments');
     }
 }
