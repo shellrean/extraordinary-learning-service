c3cd30091d07dc59c630bc8e6ca0bd5d486c4fca
Kuswandi <wandinak17@gmail.com>
2020-09-26 21:52:13 +0700

    reporting absent range change excel

--- app/Http/Controllers/Api/v1/ReportController.php
+++ app/Http/Controllers/Api/v1/ReportController.php
@@ -3,22 +3,24 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Repositories\ClassroomRepository;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use App\Repositories\ReportRepository;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Controllers\Controller;
+use App\Exports\RecapAbcentSpreet;
 use App\Exports\RecapAbcentExport;
 use App\Actions\SendResponse;
 
 class ReportController extends Controller
 {
-   /**
+    /**
     * Get recapitulation abcent
     * 
     * @author shellrean <wandinak17@gmail.com>
     * @param $classroom_id
     * @return \App\Actions\SendResponse
     */
-    public function recapAbcent(ReportRepository $reportRepository, ClassroomRepository $classroomRepository) 
+    public function recapAbcentExcel(ReportRepository $reportRepository, ClassroomRepository $classroomRepository) 
     {
         $classroom_id = isset(request()->c) && request()->c != ''
                 ? request()->c
@@ -57,12 +59,20 @@ class ReportController extends Controller
         $reportRepository->getDataRecapAbcents($classroom_id, $schedule_id, $from, $end);
 
         $results = $reportRepository->getRecapAbcents();
+
         $begin = new \DateTime($from->format('Y-m-d'));
         $end = new \DateTime($end->format('Y-m-d'));
 
         $interval = new \DateInterval('P1D');
         $daterange = new \DatePeriod($begin, $interval ,$end);
 
-        return Excel::download(new RecapAbcentExport($results, $daterange, $classroomRepository->getSchedule()->day), 'recapitulasi_abcent.xlsx');
+        $spreadsheet = RecapAbcentSpreet::export($results, $daterange, $classroomRepository->getSchedule()->day, $from, $end, $classroomRepository->getSchedule());
+        $writer = new Xlsx($spreadsheet);
+        
+
+        $filename = 'Rekapitulasi absensi dari '.$from->format('d-m-Y').'_'.$end->format('d-m-Y');
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment; filename="'.$filename.'.xlsx"');
+        $writer->save('php://output');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\v1'], function() {
 	Route::get('report/today', 'AbcentController@reportToday')->middleware('auth.res');
 
 	Route::get('download/excel/standart', 'StandartController@exportExcel');
+	Route::get('download/excel/recap-abcent', 'ReportController@recapAbcentExcel');
 	
 	Route::group(['middleware' => 'auth:api'], function() {
 		Route::get('logout', 'AuthController@logout');
